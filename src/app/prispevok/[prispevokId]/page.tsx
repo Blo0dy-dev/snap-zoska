cad67d2f3176782e186fe54f0dccb6de8b1d40ee
Blo0dy-dev <filip.foldvary@gmail.com>
2024-10-03 10:58:37 +0200

    spravenie detailu prispevkuId

---
+++ src/app/prispevok/[prispevokId]/page.tsx
@@ -0,0 +1,18 @@
+// src/app/prispevok/[prispevokId]/page.tsx
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = {title: "Detail príspevku | Zoska"}
+
+export default function PostDetail({
+  params, 
+}: {
+  params: {prispevokId: string};
+}) {
+  
+  return (
+      
+      <Typography> Detail konkrétneho prispevku {params.prispevokId}  </Typography>
+      
+  );
+}
